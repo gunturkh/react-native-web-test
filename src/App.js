54be0ae47c9c7a6f34affe9644aee96c8c28cbdb
Guntur KH <guntur.kh@gmail.com>
2024-06-21 09:29:00 +0700

    feat: react-native-web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  useWindowDimensions,
+} from 'react-native';
 
-function App() {
+export default function App() {
+  const {height} = useWindowDimensions();
+  const [number, setNumber] = useState(0);
+
+  function handlePress() {
+    setNumber(parseInt(Math.random() * 10000, 10) % 100);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={[styles.container, {height}, StyleSheet.absoluteFill]}>
+      <Text>Random number: {number}</Text>
+      <View style={styles.br} />
+      <Pressable
+        style={({pressed}) => [
+          {
+            opacity: pressed ? 0.7 : 1,
+          },
+          styles.btn,
+        ]}
+        onPress={handlePress}>
+        <Text style={styles.btnText}>Generate a number</Text>
+      </Pressable>
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  br: {
+    height: 12,
+  },
+  btn: {
+    backgroundColor: '#222',
+    padding: 10,
+  },
+  btnText: {
+    color: '#fff',
+  },
+});
